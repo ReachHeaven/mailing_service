77008f00b78b36dd378b5ab8c1a71382b0fe0844
Amirov.Karim <karamirov@gmail.com>
2022-12-16 12:59:54 +0600

    Add environments, dockerfile, post handler

--- maillingsite/mailling/admin.py
+++ maillingsite/mailling/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import Mailing, Message, Client
 # Register your models here.
 admin.site.register(Mailing)
+admin.site.register(Message)
+admin.site.register(Client)
--- maillingsite/mailling/serializer.py
+++ maillingsite/mailling/serializer.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Mailing
+from .models import Mailing, Client, Message
 
 
 class MallingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mailing
+        fields = "content"
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = "content"
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
         fields = "content"
